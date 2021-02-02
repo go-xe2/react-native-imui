a92dd9994acc1b4cd6d787f1e4c58e60222021dd
yangtxiang <279197148@qq.com>
2021-02-02 18:46:35 +0800

    整理文件

--- example/App.js
+++ example/App.js
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  KeyboardAvoidingView,
 } from 'react-native';
 
 import {
@@ -26,19 +27,70 @@ import {
 import IMUI from 'react-native-imui';
 
 //
-// const ChatInput = IMUI.ChatInput;
+const ChatInput = IMUI.ChatInput;
 const EmojiView = IMUI.EmojiView;
-console.log('EmojiView:', EmojiView);
+const IMInput = IMUI.IMInput;
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  onSelect = (data) => {
+    console.log('onSelect:', data);
+  };
+  onDelete =(data) => {
+    console.log('onDelete:', data);
+  };
+  onPageChanged = (data) => {
+    console.log('onPageChanged:', data);
+  };
+  onInputChange = (v) => {
+  };
+  onInputViewSizeChange = (size) => {
+    if (this.state.inputLayoutHeight !== size.height) {
+      this.setState({
+        inputLayoutHeight: size.height,
+      });
+    }
+  }
+  componentDidMount() {
+    if (this.imInputRef.current) {
+      this.imInputRef.current.insertText('this is insert text');
+    }
+  }
+
+  imInputRef = React.createRef();
   render () {
+    const { inputLayoutHeight = 360, text = 'this is init text.' } = this.state;
     return (
-        <View>
-          <Text>this is demo.</Text>
-        </View>
+          <KeyboardAvoidingView behavior={'padding'} style={{flex: 1}} >
+          <View>
+            <ChatInput style={{ width: 360, height: inputLayoutHeight, backgroundColor: '#f3f3f3' }}
+                 inputPadding={{ left: 30, top: 10, right: 10, bottom: 10 }}
+                 galleryScale={0.6}
+                 compressionQuality={0.6}
+                 cameraQuality={0.7}
+                  onSizeChange={this.onInputViewSizeChange}
+                 customLayoutItems={{
+                   left: ['voice'],
+                   right: ['send'],
+                   bottom: ['gallery', 'emoji', 'camera', 'file'],
+                 }}
+                 showSelectAlbumBtn
+                 showRecordVideoBtn
+            />
+            <IMInput ref={this.imInputRef} style={{ width: 300, height: 60, backgroundColor: '#f1f1f1', borderWidth: 1, borderColor: 'red', margin: 20, borderRadius: 10, padding: 10 }}
+                     onChange={this.onInputChange}
+                     text={text}
+            />
+            <EmojiView
+                style={{ width: 360, height: 300, backgroundColor: 'red' }}
+                onSelect={this.onSelect}
+                onPageChanged={this.onPageChanged}
+                onDelete={this.onDelete}
+            />
+          </View>
+          </KeyboardAvoidingView>
     );
   }
 }
